1fc33edbd10c3d73e061f04a50d0614cc1b0dbe1
alrare <alrare@hotmail.com>
2024-03-18 20:38:10 -0600

    firts commit

---
+++ SerilogConsoleFramework4.6/Console/Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.File(@"C:\\Users\\Propietario\\Desktop\\SerilogConsoleFramework4.6\\Console\\Console\\Logger\\Log.txt").CreateLogger();
+
+            Log.Information("Este es un log de prueba de Consola con Net Framework 4.6");
+
+            try 
+            {
+                int a = 2;
+                int b = 0;
+                Log.Warning("Los valores son {0} and {1}", a, b);
+                int c = a / b;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Ocurrio un error");
+            }
+
+            Log.CloseAndFlush();
+
+            //Console.WriteLine("Completado");
+            //Console.ReadKey();
+
+        }
+    }
+}
---
+++ SerilogWebApiFramework4.6/WebApi/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            var logpath = System.Web.Hosting.HostingEnvironment.MapPath("~/Logger/Log.txt");
+
+            var log = new LoggerConfiguration()
+                .WriteTo.File(logpath)
+                .CreateLogger();
+
+            log.Information("Este es un log de prueba de WebApi con Net Framework 4.6");
+
+            try
+            {
+                int a = 2;
+                int b = 0;
+                log.Warning("Los valores son {0} and {1}", a, b);
+                int c = a / b;
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex, "Ocurrio un error");
+            }
+
+            return View();
+        }
+    }
+}
